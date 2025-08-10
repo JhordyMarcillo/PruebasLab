c5e1e93e634da84d505d3d16b94f31f280be17d2
JhordyMarcillo <jhordypaulsb2@gmail.com>
2025-08-10 13:28:40 -0500

    Update

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000;
 
 //endpoint de respuesta 
 app.get('/', (_req, res) => {
-  res.send('Integración continua en Express.js');
+  res.send('Integración');
 });
 
 
