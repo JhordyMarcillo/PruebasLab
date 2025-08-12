b97b234df325ddf0a926f7f03f5c042fe42b37ab
JhordyMarcillo <jhordypaulsb2@gmail.com>
2025-08-12 08:01:18 -0500

    configuracion firebase

--- math.test.js
+++ math.test.js
@@ -2,7 +2,7 @@ const { factorial, fibonacci } = require('./math');
 
 describe('Pruebas para factorial', () => {
     test('Factorial de 0 debe ser 1', () => {
-        expect(factorial(0)).toBe(0);
+        expect(factorial(0)).toBe(1);
     });
 
     test('Factorial de 5 debe ser 120', () => {
@@ -12,7 +12,7 @@ describe('Pruebas para factorial', () => {
 
 describe('Pruebas para fibonacci', () => {
     test('Fibonacci de 0 debe ser 1', () => {
-        expect(fibonacci(0)).toBe(1);
+        expect(fibonacci(0)).toBe(0);
     });
 
     test('Fibonacci de 6 debe ser 8', () => {
